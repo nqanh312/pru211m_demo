9b69fa7c6da56483131712d431fa73e5d3f6436c
unknown <QAnh@DESKTOP-3FU2J2V.fun.cantho>
2023-05-21 12:11:37 +0700

    9

---
+++ Run2D/Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollower : MonoBehaviour
+{
+
+    [SerializeField] private 
+    private void Update()
+    {
+        
+    }
+}
